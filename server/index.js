6fd97e7fdfa4fe7c2676623afad7c4cc184e981f
unknown <shahsank1995@gmail.com>
2025-11-29 14:05:07 +0530

    Fix CORS: Allow x-user-id header

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const corsOptions = {
         ? ['https://warranties-app.vercel.app', 'https://www.warranties-app.vercel.app']
         : ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-user-id'],
     credentials: true
 };
 app.use(cors(corsOptions));
